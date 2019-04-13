0efdb49622c7f013f41fa9edc8038f0f40cc59ef
Poly Goveia <poly.goveia@gmail.com>
2019-04-13 09:52:20 -0700

    add 404 error routes using switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Header from './Header'
 import Home from './Home'
 import About from './About'
 import Teachers from './Teachers'
 import Courses from './Courses'
+import NotFound from './NotFound';
 
 const App = () => (
   <div className="container">
     <Header/>
-    <Route exact path="/" component={Home}/>
-    <Route path="/about" component={About}/>
-    <Route path="/teachers" component={Teachers}/>
-    <Route path="/courses" component={Courses}/>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/teachers" component={Teachers}/>
+      <Route path="/courses" component={Courses}/>
+      <Route component={NotFound} />
+    </Switch>
   </div>
 );
 
